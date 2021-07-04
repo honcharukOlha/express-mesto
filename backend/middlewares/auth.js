556c63f1df83c4ada7567d2680fdd9eb05419c24
Goncharuk <OlyaGoncharuk@yandex.ru>
2021-07-04 15:36:47 +0300

    исправлена ошибка в ответе для для неавт-х польз.

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new AuthorizeError({ message: 'Необходима авторизация' }));
+    next(new AuthorizeError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     // отправляем ошибку, если не получилось
-    next(new AuthorizeError({ message: 'Необходима авторизация' }));
+    next(new AuthorizeError('Необходима авторизация'));
   }
   req.user = payload; // записываем пейлоуд в объект запроса
   return next(); // пропускаем запрос дальше
